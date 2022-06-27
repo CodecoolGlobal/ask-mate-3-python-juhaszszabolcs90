5e27846c40fef75e7e8dadd48ce36339ce29c6e3
eszterberenyi <e.eszterberenyi@gmail.com>
2022-06-27 16:07:10 +0200

    add basic routing logic(/, /list, /questions@id) /add-question started -- NOT TESTED

--- server.py
+++ server.py
@@ -1,12 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import connnection
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html") #inheritence template
+
+
+@app.route("/list", methods=["GET"])
+def display_questions():
+    return render_template(
+        "questions.html",
+        questions=connection.read_data(questions)
+    )
+
+
+@app.route("/question/<question_id>", methods=["GET"])
+def display_question():
+    return render_template(
+        "question.html",
+        questions=connection.read_data(questions),
+        answers=connection.read_data(answers)
+    )
+
+
+@app.route("/add-question", methods=["GET", "POST"]
+def add_question():
+    if request.method == "GET":
+        return render_template(
+            "add_question.html",
+        )
+
+    return redirect(url_for("display_question"))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        debug=True
+        port=5000
+    )
