862aa00c993da9802a2a3ad8e90f2bc677b572b7
eszterberenyi <e.eszterberenyi@gmail.com>
2022-06-29 09:52:50 +0200

    merge

--- connection.py
+++ connection.py
@@ -1,16 +1,14 @@
 import csv
-import os
 
 DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 
-def read_questions(filename):
+def read_data(filename):
     with open(filename, newline='') as questions:
-        questions = list(csv.DictReader(questions, delimiter=","))
-    return questions
+    return list(csv.DictReader(questions, delimiter=","))
 
 
-def write_questions(filename, data: dict):
-    with open(filename, 'a', newline='') as datas:
-        writer = csv.DictWriter(datas, DATA_HEADER)
-        writer.writerow(data)
+def write_data(filename, data):
+    with open(filename, 'a', newline='') as writer:
+        writer = csv.DictWriter(writer, DATA_HEADER)
+        writer.writerows(data)
--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 import connection
-import util
 
 app = Flask(__name__)
 
@@ -12,8 +11,6 @@ def hello():
 
 @app.route("/list")
 def display_questions():
-    if request.method == 'POST':
-        return redirect(url_for('add_question'))
     questions = connection.read_questions(filename="sample_data/question.csv")
     headers = connection.DATA_HEADER
     return render_template('questions.html', questions=questions, headers=headers)
@@ -30,25 +27,27 @@ def display_question():
     )
 
 
-@app.route('/add_question', methods=['GET', 'POST'])
-def add_question():
-    filename = "sample_data/question.csv"
+@app.route('/add_question/<id>', methods=['GET','POST'])
+def story():
     if request.method == 'POST':
-        data = {}
-        data['id'] = util.generate_id(filename)
-        data['submission_time'] = "2"
-        data['view_number'] = '10'
-        data['vote_number'] = '5'
-        data['title'] = request.form['title']
-        data['message'] = request.form['message']
-        data['image'] = request.form['image']
+        user_story = {}
+        user_story['id'] = data_handler.generate_id()
+        user_story['title'] = request.form['title']
+        user_story['user_story'] = request.form['story']
+        user_story['acceptance_criteria'] = request.form['acceptance-criteria']
+        user_story['business_value'] = request.form['value']
+        user_story['estimation'] = request.form['estimation']
+        user_story['status'] = 'planning'
 
-        connection.write_questions(filename, data)
-        return redirect(url_for('display_questions'))
-    return render_template('add_question.html')
+        data_handler.add_user_story(user_story)
+        return redirect('/')
+    return render_template('story.html')
 
+@app.route("/add-question", methods=["GET", "POST"])
+def add_question():
+    return render_template("add_question.html")
 
-"""""
+"""
 @app.route("/add-question", methods=["GET", "POST"])
 def add_question():
     if request.method == "GET":
@@ -56,6 +55,7 @@ def add_question():
     data_manager.write_to_file(questions, request.form)
     return redirect(url_for("display_question")) #---> a saját, most generált ID-ja kell a kérdésnek
 """
+
 
 
 """
@@ -66,6 +66,7 @@ def add_answer():
     data_manager.write_to_file(answers, request.form)
     return redirect(url_for("display_question"))
 """
+
 
 
 if __name__ == "__main__":
