e03b0bb5f72faa1405f48f755b635e533279caeb
eszterberenyi <e.eszterberenyi@gmail.com>
2022-06-30 10:30:00 +0200

    code cleanup in server.py

--- server.py
+++ server.py
@@ -12,6 +12,9 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 UPLOAD_FOLDER = 'static/images'
 
+QUESTION_FILE_PATH = "sample_data/question.csv"
+ANSWER_FILE_PATH = "sample_data/answer.csv"
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -22,23 +25,21 @@ def allowed_file(filename):
 
 @app.route("/")
 def hello():
-    return render_template("index.html") #inheritence template
+    return render_template("index.html")
 
 
 @app.route("/list")
 def display_questions():
     if request.method == 'POST':
         return redirect(url_for('add_question'))
-    questions = connection.read_data(filename="sample_data/question.csv")
+    questions = connection.read_data(filename=QUESTION_FILE_PATH)
     headers = connection.DATA_HEADER
     return render_template('questions.html', questions=questions, headers=headers)
-
-    #return render_template("questions.html") #,questions=connection.read_data(questions))
 
 
 @app.route("/question/<question_id>", methods=["GET"])
 def display_question(question_id):
-    questions = connection.read_data('sample_data/question.csv')
+    questions = connection.read_data(QUESTION_FILE_PATH)
     for row in questions:
         if row['id'] == question_id:
             question_to_be_displayed = row
@@ -48,7 +49,7 @@ def display_question(question_id):
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
-    file_name = "sample_data/question.csv"
+    file_name = QUESTION_FILE_PATH
     if request.method == 'POST':
         data = {
             'id': util.generate_id(file_name),
@@ -66,8 +67,6 @@ def add_question():
         connection.append_data(file_name, data)
         return redirect(url_for('display_questions'))
 
-        # id = data.get('id')
-        # return redirect(url_for(f'display_question({id})'))
     return render_template('add_question.html')
 
 
@@ -91,23 +90,23 @@ def add_answer():
 
 @app.route("/answer/<answer_id>/vote-up", methods=['GET'])
 def vote_answer_up(id):
-    util.vote("sample_data/answer.csv")
+    util.vote(ANSWER_FILE_PATH)
     return redirect(url_for(f'display_question({id})'))
 
 @app.route("/answer/<answer_id>/vote-down", methods=['GET'])
 def vote_answer_down(id):
-    util.vote("sample_data/answer.csv", False)
+    util.vote(ANSWER_FILE_PATH, False)
     return redirect(url_for(f'display_question({id})'))
 
 
 @app.route("/question/<question_id>/vote-up", methods=['GET'])
 def vote_question_up(id):
-    util.vote("sample_data/question.csv")
+    util.vote(QUESTION_FILE_PATH)
     return redirect("/list")
 
 @app.route("/question/<question_id>/vote-down", methods=['GET'])
 def vote_question_down(id):
-    util.vote("sample_data/question.csv", False)
+    util.vote(QUESTION_FILE_PATH, False)
     return redirect("/list")
 
 
