c34b7195fa53097ad117cedbcf5a225d2e2864dd
eszterberenyi <e.eszterberenyi@gmail.com>
2022-07-25 20:20:47 +0200

    start converting db functionality

--- data_manager.py
+++ data_manager.py
@@ -1,36 +1,67 @@
-import connection
-import csv
-import operator
+from typing import List, Dict
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
 
-def update_data(filename, data, headers):
-    with open(filename, 'w', newline='') as f:
-        writer = csv.DictWriter(f, headers)
-        writer.writeheader()
-        writer.writerows(data)
+import Database_connection
+
+@Database_connection.connection_handler
+def get_questions(cursor):
+    query = """
+        SELECT
+        submission_time AS date,
+        view_number AS views,
+        vote_number As votes,
+        title,
+        message,
+        image
+        FROM question;"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+# select // from question DISPLAY QUESTIONS
+# select // from question where id join answer question id question id
+# insert into question ADD Q
+# select id from question order by id desc limit 1 ADD QUESTION
+# DELETE from question WHERE question_id LIKE '%{delete_id}%'
+# UPDATE question SET question = '{updated_question}' WHERE question_id = '{question_id}'
 
 
-def delete_question(id_question):
-    questions = connection.read_data('sample_data/question.csv')
-    filtered_questions = filter(lambda question: question['id'] != id_question, questions)
-    connection.write_data('sample_data/question.csv', list(filtered_questions))
+# @Database_connection.connection_handler
+# def get_question(cursor)
+#query = """
+        #SELECT
+
+# cursor.execute(query
+# #return cursor.fetchone()
 
 
-def should_delete_question(id_question):
-    questions = connection.read_data('sample_data/question.csv')
-
-
-def sort_data(data, sort_by='submission_time', reverse=False):
-    # for d in data:
-    #     try:
-    #         for k, v in d.items():
-    #             d[k] = int(v)
-    #     except ValueError:
-    #         for k, v in d.items():
-    #             d[k] = str(v).lower()
-    # print(data)
-    return sorted(data, key=operator.itemgetter(sort_by), reverse=reverse)
-
-def delete_answer(id_answer):
-    answers = connection.read_data('sample_data/answer.csv')
-    filtered_answers = filter(lambda answer: answer['id'] != id_answer, answers)
-    connection.write_data('sample_data/answer.csv', list(filtered_answers))
+# def update_data(filename, data, headers):
+#     with open(filename, 'w', newline='') as f:
+#
+#
+#
+# def delete_question(id_question):
+#     questions = connection.read_data('sample_data/question.csv')
+#     filtered_questions = filter(lambda question: question['id'] != id_question, questions)
+#     connection.write_data('sample_data/question.csv', list(filtered_questions))
+#
+#
+# def should_delete_question(id_question):
+#     questions = connection.read_data('sample_data/question.csv')
+#
+#
+# def sort_data(data, sort_by='submission_time', reverse=False):
+#     # for d in data:
+#     #     try:
+#     #         for k, v in d.items():
+#     #             d[k] = int(v)
+#     #     except ValueError:
+#     #         for k, v in d.items():
+#     #             d[k] = str(v).lower()
+#     # print(data)
+#     return sorted(data, key=operator.itemgetter(sort_by), reverse=reverse)
+#
+# def delete_answer(id_answer):
+#     answers = connection.read_data('sample_data/answer.csv')
+#     filtered_answers = filter(lambda answer: answer['id'] != id_answer, answers)
+#     connection.write_data('sample_data/answer.csv', list(filtered_answers))
--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-import connection
 import data_manager
-import util
+# import util
 
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,8 +11,8 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 UPLOAD_FOLDER = 'static/images'
 
-QUESTION_FILE_PATH = "sample_data/question.csv"
-ANSWER_FILE_PATH = "sample_data/answer.csv"
+# QUESTION_FILE_PATH = "sample_data/question.csv"
+# ANSWER_FILE_PATH = "sample_data/answer.csv"
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -30,159 +29,150 @@ def allowed_file(filename):
 @app.route("/")
 @app.route("/list")
 def display_questions():
-    order_by = request.args.get('order_by')
-    order = request.args.get('order')
-    if request.method == 'POST':
-        return redirect(url_for('add_question'))
-    questions = connection.read_data(filename=QUESTION_FILE_PATH)
-    for question in questions:
-        question['submission_time'] = util.convert_timestamp(float(question.get('submission_time')))
-    if not order_by:
-        data_manager.sort_data(questions)
-    else:
-        questions = data_manager.sort_data(questions, order_by, bool(int(order)))
-    headers = util.convert_headers(connection.DATA_HEADER)
-    order_by_headers = connection.DATA_HEADER
-    return render_template('questions.html', questions=questions, headers=headers[1:], order_by_headers=order_by_headers[1:6])
+    questions = data_manager.get_questions()
+    print(questions)
+    # if request.method == 'POST':
+    #     return redirect(url_for('add_question'))
+    return render_template('questions.html', questions=questions)
 
 
-@app.route("/question/<question_id>", methods=["GET", 'POST'])
-def display_question(question_id):
-    questions = connection.read_data(QUESTION_FILE_PATH)
-    answers = connection.read_data('sample_data/answer.csv')
-    headers = util.convert_headers(connection.ANSWER_HEADER)
-    answers_to_be_displayed = []
-    question_to_be_displayed = ""
-    for row in questions:
-        if row['id'] == question_id:
-            question_to_be_displayed = row
-            view_num = int(row['view_number'])
-            view_num += 1
-            row['view_number'] = str(view_num)
-            data_manager.update_data(QUESTION_FILE_PATH, questions, connection.DATA_HEADER)
-    for column in answers:
-        if column['question_id'] == question_id:
-            answers_to_be_displayed.append(column)
-    for answer in answers_to_be_displayed:
-        answer['submission_time'] = util.convert_timestamp(float(answer.get('submission_time')))
+# @app.route("/question/<question_id>", methods=["GET", 'POST'])
+# def display_question(question_id):
+#     question = data_manager.get_question
+#
+#     # answers_to_be_displayed = []
+#     # question_to_be_displayed = ""
+#     # for row in questions:
+#     #     if row['id'] == question_id:
+#     #         question_to_be_displayed = row
+#     #         view_num = int(row['view_number'])
+#     #         view_num += 1
+#     #         row['view_number'] = str(view_num)
+#     #         data_manager.update_data(QUESTION_FILE_PATH, questions, connection.DATA_HEADER)
+#     # for column in answers:
+#     #     if column['question_id'] == question_id:
+#     #         answers_to_be_displayed.append(column)
+#     # for answer in answers_to_be_displayed:
+#     #     answer['submission_time'] = util.convert_timestamp(float(answer.get('submission_time')))
+#
+#     return render_template("display_question.html", question=question_to_be_displayed, question_id=question_id, answers=answers_to_be_displayed, headers=headers)
+#
+#
+#
+# @app.route('/add_question/', methods=['GET','POST'])
+# def add_question():
+#     # file_name = "sample_data/question.csv"
+#     if request.method == 'POST':
+#         data = {
+#             # 'id': util.generate_id(file_name),
+#             TODO 'submission_time': util.generate_timestamp(),
+#             'view_number': '0',
+#             'vote_number': '0',
+#             'title': request.form.get('title', ''),
+#             'message': request.form.get('message', ''),
+#             'image': 'images/%s' % request.files.get('image', '').filename
+#         }
+#         file = request.files['image']
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         # connection.append_data(file_name, data)
+#         id = TODO
+#         return redirect(url_for('display_question', question_id=id))
+#     return render_template('add_question.html')
+#
+#
+#
+# @app.route("/question/<question_id>/delete", methods=["GET", "POST"])
+# def delete_question(question_id):
+#     if request.method == 'POST':
+#         # data_manager.delete_question(question_id)
+#
+#     return redirect(url_for('display_questions'))
+#
+# @app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
+# def edit_question(question_id):
+#     # questions = connection.read_data(QUESTION_FILE_PATH)
+#     # for question in questions:
+#     #     if question['id'] == question_id:
+#     #         row = question
+#     if request.method == 'POST':
+#         for question in questions:
+#             if question['id'] == question_id:
+#                 question['title'] = request.form['title']
+#                 question['message'] = request.form['message']
+#                 question['image'] = 'images/%s' % request.files.get('image', '').filename
+#                 file = request.files['image']
+#                 if file and allowed_file(file.filename):
+#                     filename = secure_filename(file.filename)
+#                     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#                 data_manager.update_data(QUESTION_FILE_PATH, questions, connection.DATA_HEADER)
+#                 question_id = question['id']
+#                 return redirect(url_for('display_question', question_id=question_id))
+#     return render_template('edit_question.html', question=row)
+#
+#
+# @app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+# def add_answer(question_id):
+#     if request.method == "GET":
+#         return render_template("add_answer.html", question_id=question_id)
+#     data = {
+#         # 'id': util.generate_id(ANSWER_FILE_PATH),
+#         'submission_time': util.generate_timestamp(),
+#         'vote_number': '0',
+#         'question_id': question_id,
+#         'message': request.form.get('message', ''),
+#         'image': 'images/%s' % request.files.get('image', '').filename
+#     }
+#     file = request.files['image']
+#     if file and allowed_file(file.filename):
+#         filename = secure_filename(file.filename)
+#         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#     connection.append_data(ANSWER_FILE_PATH, data)
+#     return redirect(url_for("display_question", question_id=question_id))
+#
+# ESZTER
 
-    return render_template("display_question.html", question=question_to_be_displayed, question_id=question_id, answers=answers_to_be_displayed, headers=headers)
-
-
-
-@app.route('/add_question/', methods=['GET','POST'])
-def add_question():
-    file_name = "sample_data/question.csv"
-    if request.method == 'POST':
-        data = {
-            'id': util.generate_id(file_name),
-            'submission_time': util.generate_timestamp(),
-            'view_number': '0',
-            'vote_number': '0',
-            'title': request.form.get('title', ''),
-            'message': request.form.get('message', ''),
-            'image': 'images/%s' % request.files.get('image', '').filename
-        }
-        file = request.files['image']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        connection.append_data(file_name, data)
-        id = data.get('id')
-        return redirect(url_for('display_question', question_id=id))
-    return render_template('add_question.html')
-
-
-
-@app.route("/question/<question_id>/delete", methods=["GET", "POST"])
-def delete_question(question_id):
-    if request.method == 'POST':
-        data_manager.delete_question(question_id)
-
-    return redirect(url_for('display_questions'))
-
-@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
-def edit_question(question_id):
-    questions = connection.read_data(QUESTION_FILE_PATH)
-    for question in questions:
-        if question['id'] == question_id:
-            row = question
-    if request.method == 'POST':
-        for question in questions:
-            if question['id'] == question_id:
-                question['title'] = request.form['title']
-                question['message'] = request.form['message']
-                question['image'] = 'images/%s' % request.files.get('image', '').filename
-                file = request.files['image']
-                if file and allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                data_manager.update_data(QUESTION_FILE_PATH, questions, connection.DATA_HEADER)
-                question_id = question['id']
-                return redirect(url_for('display_question', question_id=question_id))
-    return render_template('edit_question.html', question=row)
-
-
-@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
-def add_answer(question_id):
-    if request.method == "GET":
-        return render_template("add_answer.html", question_id=question_id)
-    data = {
-        'id': util.generate_id(ANSWER_FILE_PATH),
-        'submission_time': util.generate_timestamp(),
-        'vote_number': '0',
-        'question_id': question_id,
-        'message': request.form.get('message', ''),
-        'image': 'images/%s' % request.files.get('image', '').filename
-    }
-    file = request.files['image']
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    connection.append_data(ANSWER_FILE_PATH, data)
-    return redirect(url_for("display_question", question_id=question_id))
-
-
-@app.route("/answer/<answer_id>/vote-up", methods=['GET'])
-def vote_answer_up(answer_id):
-    util.vote(ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id)
-    data = connection.read_data(ANSWER_FILE_PATH)
-    for d in data:
-        if d['id'] == answer_id:
-            question_id = d['question_id']
-    return redirect(url_for('display_question', question_id=question_id))
-
-
-@app.route("/answer/<answer_id>/vote-down", methods=['GET'])
-def vote_answer_down(answer_id):
-    util.vote(ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id, False)
-    data = connection.read_data(ANSWER_FILE_PATH)
-    for d in data:
-        if d['id'] == answer_id:
-            question_id = d['question_id']
-    return redirect(url_for('display_question', question_id=question_id))
-
-
-@app.route("/question/<question_id>/vote-up", methods=['GET'])
-def vote_question_up(question_id):
-    util.vote(QUESTION_FILE_PATH, connection.DATA_HEADER, question_id)
-    return redirect("/list")
-
-@app.route("/question/<question_id>/vote-down", methods=['GET'])
-def vote_question_down(question_id):
-    util.vote(QUESTION_FILE_PATH, connection.DATA_HEADER, question_id, False)
-    return redirect("/list")
-
-
-@app.route("/answer/<answer_id>/delete", methods=["GET", "POST"])
-def delete_answer(answer_id):
-    answers = connection.read_data('sample_data/answer.csv')
-    for row in answers:
-        if row['id'] == answer_id:
-            displayed_question = row
-    if request.method == 'POST':
-        data_manager.delete_answer(answer_id)
-        # return redirect(url_for('/list'))
+# @app.route("/answer/<answer_id>/vote-up", methods=['GET'])
+# def vote_answer_up(answer_id):
+#     util.vote(ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id)
+#     data = connection.read_data(ANSWER_FILE_PATH)
+#     for d in data:
+#         if d['id'] == answer_id:
+#             question_id = d['question_id']
+#     return redirect(url_for('display_question', question_id=question_id))
+#
+#
+# @app.route("/answer/<answer_id>/vote-down", methods=['GET'])
+# def vote_answer_down(answer_id):
+#     util.vote(ANSWER_FILE_PATH, connection.ANSWER_HEADER, answer_id, False)
+#     data = connection.read_data(ANSWER_FILE_PATH)
+#     for d in data:
+#         if d['id'] == answer_id:
+#             question_id = d['question_id']
+#     return redirect(url_for('display_question', question_id=question_id))
+#
+#
+# @app.route("/question/<question_id>/vote-up", methods=['GET'])
+# def vote_question_up(question_id):
+#     util.vote(QUESTION_FILE_PATH, connection.DATA_HEADER, question_id)
+#     return redirect("/list")
+#
+# @app.route("/question/<question_id>/vote-down", methods=['GET'])
+# def vote_question_down(question_id):
+#     util.vote(QUESTION_FILE_PATH, connection.DATA_HEADER, question_id, False)
+#     return redirect("/list")
+#
+#
+# @app.route("/answer/<answer_id>/delete", methods=["GET", "POST"])
+# def delete_answer(answer_id):
+#     answers = connection.read_data('sample_data/answer.csv')
+#     for row in answers:
+#         if row['id'] == answer_id:
+#             displayed_question = row
+#     if request.method == 'POST':
+#         data_manager.delete_answer(answer_id)
+#         # return redirect(url_for('/list'))
 
 
 if __name__ == "__main__":
